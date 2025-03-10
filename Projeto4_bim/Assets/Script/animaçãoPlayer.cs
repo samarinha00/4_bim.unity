c3173b8d507f881936d84a27fec1a4f175635647
samarinha00 <salapjdm224@gmail.com>
2025-03-10 10:42:35 -0300

    quase la

--- Projeto4_bim/Assets/Script/animaçãoPlayer.cs
+++ Projeto4_bim/Assets/Script/animaçãoPlayer.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class animaçãoPlayer : MonoBehaviour
 {
-    [SerializeField]
-    private Animator animator;
+    [SerializeField] private Animator animator;
 
-    [SerializeField] 
-    private Rigidbody2D rigidbody;
-    
-    
+    [SerializeField] private Rigidbody2D rigidbody;
+
+    [SerializeField] private Player player;
+
     // Update is called once per frame
     void Update()
     {
-        float velocidadeX = Mathf.Abs(this.rigidbody.velocity.x);
-        if (velocidadeX > 0) {
-            this.animator.SetBool("Andando", true);
+        if (this.player.noChao)
+        {
+            float velocidadeX = Mathf.Abs(this.rigidbody.velocity.x);
+            if (velocidadeX > 0)
+            {
+                this.animator.SetBool("Andando", true);
+            }
+            else
+            {
+                this.animator.SetBool("Andando", false);
+            }
         }
         else
         {
-            this.animator.SetBool("Andando", false);
+            float velocidadeY = this.rigidbody.velocity.y;
+            if (velocidadeY > 0)
+            {
+                //Subindo e Pulando
+                this.animator.SetBool("Jump", true);
+            }
+            else
+            {
+                this.animator.SetBool("Jump", false);
+            }
         }
     }
 }
